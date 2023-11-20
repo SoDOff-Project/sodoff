e956985678946b3902b640182d4600c81cd8408f
Alan Moon <alanmoonbase2004@gmail.com>
2023-11-20 15:24:43 -0800

    Add Placeholder For LogEvent
    and Add ``ping`` Method For Status Page

---
+++ src/Controllers/Common/AnalyticsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sodoff.Controllers.Common
+{
+    public class AnalyticsController : Controller
+    {
+        [HttpPost]
+        [Produces("application/xml")]
+        [Route("AnalyticsWebService.asmx/LogEvent")]
+        public IActionResult LogEvent()
+        {
+            // placeholder to prevent a ton of 404's
+            return Ok(true);
+        }
+
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
