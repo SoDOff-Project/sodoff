1b2622927f6e1c7fef5580bdcf95d730d82ea476
Hipposgrumm <hipposgrumm@gmail.com>
2024-12-02 22:52:31 -0700

    Eat My Dust support DB MODEL CHANGES! I don't know how to deal with those. Support for EMD's MMO rooms and API token. Groundwork for group/clan system (hence schema changes). Only functional for EMD right now (if I implemented it correctly).

---
+++ src/Schema/Group.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml.Serialization;
+
+namespace sodoff.Schema;
+
+[Serializable]
+[XmlRoot(ElementName = "GP", IsNullable = true)]
+public class Group {
+	[XmlElement(ElementName = "G", IsNullable = false)]
+	public string GroupID;
+
+	[XmlElement(ElementName = "N", IsNullable = false)]
+	public string Name;
+
+	[XmlElement(ElementName = "D", IsNullable = false)]
+	public string Description;
+
+	[XmlElement(ElementName = "T", IsNullable = false)]
+	public GroupType Type;
+
+	[XmlElement(ElementName = "O", IsNullable = true)]
+	public string OwnerID;
+
+	[XmlElement(ElementName = "L", IsNullable = true)]
+	public string Logo;
+
+	[XmlElement(ElementName = "C", IsNullable = true)]
+	public string Color;
+
+	[XmlElement(ElementName = "M", IsNullable = true)]
+	public int? MemberLimit;
+
+	[XmlElement(ElementName = "TC", IsNullable = true)]
+	public int? TotalMemberCount;
+
+	[XmlElement(ElementName = "A", IsNullable = false)]
+	public bool Active;
+
+	[XmlElement(ElementName = "P", IsNullable = true)]
+	public string ParentGroupID;
+
+	[XmlElement(ElementName = "PS", IsNullable = true)]
+	public int? Points;
+
+	[XmlElement(ElementName = "RK", IsNullable = true)]
+	public int? Rank;
+
+	[XmlElement(ElementName = "RT", IsNullable = true)]
+	public int? RankTrend;
+
+	[XmlElement(ElementName = "CD")]
+	public DateTime CreateDate;
+}
---
+++ src/Schema/JoinGroupResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml.Serialization;
+
+namespace sodoff.Schema;
+
+[XmlRoot(ElementName = "JoinGroupResult", IsNullable = true, Namespace = "")]
+public class JoinGroupResult {
+	[XmlElement(ElementName = "GroupStatus")]
+	public GroupMembershipStatus GroupStatus;
+}
